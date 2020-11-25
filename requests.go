3bd91702ae66f0a291d00ae40dfa03306759113c
Ehco1996 <zh19960202@gmail.com>
2020-11-25 08:50:47 +0800

    fix: fix postJson #16

--- requests.go
+++ requests.go
@@ -19,6 +19,9 @@ func postJson(c *http.Client, url string, dataStruct interface{}) error {
 	buf := new(bytes.Buffer)
 	json.NewEncoder(buf).Encode(dataStruct)
 	r, err := http.Post(url, "application/json", buf)
+	if err != nil {
+		return err
+	}
 	defer r.Body.Close()
 	return err
 }
